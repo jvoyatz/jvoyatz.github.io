5a43eb27e024f6685d721357c3c6ec80a196694f
ioannis.vogiatzis <ioannis.vogiatzis@accenture.com>
2025-02-01 01:37:32 +0200

    Add dummy vinyls section

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/vinyls', title: 'Vinyls' },
   { href: '/about', title: 'About' },
 ]
 
